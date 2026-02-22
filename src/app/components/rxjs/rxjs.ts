f10f0acf3593822c0b0c136d11c6e5930deeca24
Meera Sahib <sahib@aristostar.com>
2026-02-22 15:01:29 +0400

    interval and timer

--- src/app/components/rxjs/rxjs.ts
+++ src/app/components/rxjs/rxjs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { from, Observable, of } from 'rxjs';
+import { from, interval, Observable, of, take, timer } from 'rxjs';
 
 @Component({
   selector: 'rxjs-basic',
@@ -10,17 +10,21 @@ import { from, Observable, of } from 'rxjs';
 export class RxjsBasic {
   cityList$ = of(['Delhi', 'Mumbai', 'Bangalore', 'Chennai']);
   number$ = from ([1, 2, 3, 4, 5]);
+  interval$ = interval(1000);
+  timer$ = timer(3000, 3000);
   constructor(){
    const myObservable$ = new Observable(val => {
      val.next('Hello');
      val.next('World!');
      val.complete();
    });
-    myObservable$.subscribe({
-      next: (val) => console.log(val),
-      complete: () => console.log('Completed')
-    });
-    this.cityList$.subscribe( val => console.log(val));
-    this.number$.subscribe( val => console.log(val));
+    // myObservable$.subscribe({
+    //   next: (val) => console.log(val),
+    //   complete: () => console.log('Completed')
+    // });
+    // this.cityList$.subscribe( val => console.log(val));
+    // this.number$.subscribe( val => console.log(val));
+    this.interval$.pipe(take(10)).subscribe( val => console.log(val));
+    this.timer$.pipe(take(5)).subscribe( val => console.log('timer executed', val));
   }
 }
