6a5ef889f79119044e79077657aa8864e5dd35ba
Meera Sahib <sahib@aristostar.com>
2026-02-23 23:12:08 +0400

    Add search input to RxjsBasic component and integrate ReactiveFormsModule

--- src/app/components/rxjs/rxjs.ts
+++ src/app/components/rxjs/rxjs.ts
@@ -1,20 +1,24 @@
 import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { filter } from 'rxjs';
 import { UserService } from '../../services/user-service';
 
 @Component({
   selector: 'rxjs-basic',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './rxjs.html',
   styleUrl: './rxjs.scss'
 })
 export class RxjsBasic {
    userService = inject(UserService);
+   searchText = new FormControl();
    
    constructor(){
    
     this.userService.getUsers().subscribe(res => console.log(res));
       this.userService.getUserById(1).subscribe(res => console.log(res));
       this.userService.allUsers$.subscribe(res => console.log('All Users from Subject:', res));
+      this.searchText.valueChanges.pipe(filter(value => value.length >= 4)).subscribe(value => console.log('Search Text:', value));
    }
   
 }
