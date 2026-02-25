9e5deadb192a5d0e071c9b7217ae42a04aff2688
Meera Sahib <sahib@aristostar.com>
2026-02-25 21:58:39 +0400

    merge map

--- src/app/components/combine-obs/combine-obs.ts
+++ src/app/components/combine-obs/combine-obs.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { debounceTime, switchMap } from 'rxjs';
+import { mergeMap } from 'rxjs';
 
 @Component({
   selector: 'app-combine-obs',
@@ -27,8 +27,11 @@ export class CombineObs {
     // });
 
     this.searchProduct.valueChanges.pipe(
-      debounceTime(300),
-      switchMap((searchTerm) => this.http.get('https://dummyjson.com/products/search?q=' + searchTerm))
+      mergeMap((searchTerm) => this.http.get('https://dummyjson.com/products/search?q=' + searchTerm))
     ).subscribe(res => console.log(res));
+
+
   }
+
+
 }
