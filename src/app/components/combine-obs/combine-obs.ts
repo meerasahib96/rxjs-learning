2fc2b1eb34d07a09e3a61937017677a76b01a80f
Meera Sahib <sahib@aristostar.com>
2026-02-25 21:52:23 +0400

    switch map

--- src/app/components/combine-obs/combine-obs.ts
+++ src/app/components/combine-obs/combine-obs.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { forkJoin } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-combine-obs',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './combine-obs.html',
   styleUrl: './combine-obs.scss'
 })
@@ -13,14 +14,20 @@ export class CombineObs {
   users$ = this.http.get('https://jsonplaceholder.typicode.com/users');
   posts$ = this.http.get('https://jsonplaceholder.typicode.com/posts');
 
+  searchProduct = new FormControl()
+
   constructor() {
-    forkJoin([this.users$, this.posts$]).subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.error('Error fetching data:', err);
-      }
-    });
+    // forkJoin([this.users$, this.posts$]).subscribe({
+    //   next: (res) => {
+    //     console.log(res);
+    //   },
+    //   error: (err) => {
+    //     console.error('Error fetching data:', err);
+    //   }
+    // });
+
+    this.searchProduct.valueChanges.pipe(
+      switchMap((searchTerm) => this.http.get('https://dummyjson.com/products/search?q=' + searchTerm))
+    ).subscribe(res => console.log(res));
   }
 }
